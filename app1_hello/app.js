0a9fb29c1ffc3e3df6f83075f516e0c1433f2dd1
ANKUR <ankurvermaaxz@gmail.com>
2021-05-01 21:24:30 +0530

    Methods and v-html directive

--- app1_hello/app.js
+++ app1_hello/app.js
@@ -2,8 +2,27 @@ const app= Vue.createApp({
     data(){
         return {
             title:"Create an App",
-            link:"https://vuejs.org"
+            link:"https://vuejs.org",
+            text1:" Learn Vue",
+            text2:"Learn JS, TS, HTML, CSS",
+            text3:"Learn Web Dev",
+            text4:<h1>"Learn Git"</h1>
         }
+    },
+    methods: {
+        getText: function(){
+            return "Learn Vue"
+        },
+        getAnothertext(){
+            return "Learn JS, TS, HTML, CSS"
+        },
+        getMoreText(){
+            return this.text3;
+        },
+        getEvenMoreText(){
+            return this.text4;
+        }
+
     }
 });
 
